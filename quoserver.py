a07e3027c93880a5adbeb780b36ea88fe859a763
Davide Mancusi <d.mancusi@ulg.ac.be>
2010-04-11 20:19:45 +0200

    Give each pawn a unique hash
    Identify each of the pawns in the server by a unique 15-character hash.

--- quoserver.py
+++ quoserver.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
-from quoboard import *
+import quoboard
+import random
+global up,right,down,left,vdir
+vdir = quoboard.vdir
+up = quoboard.up
+right = quoboard.right
+down = quoboard.down
+left = quoboard.left
 
 class Pawn:
     """Pawn class"""
@@ -8,28 +15,45 @@ class Pawn:
         self.position=(x,y)
         self.symbol=symbol
         self.goal=goal
+        alphabet='abcdefghijklmnopqrstuvwxyz'
+        self.h=reduce(lambda a,b:a+b, [ random.choice(alphabet) for i in range(15) ] )
 
-class ServerBoard(Board):
+    def move(self,direction):
+       self.position=tuple(map(sum,zip(self.position,quoboard.vdir[direction])))
+       return self.position
+
+class ServerBoard(quoboard.Board):
     """Board class to be used by server applications
     Includes methods to add barriers and update the table of allowed moves"""
 
     def __init__(self,side=9,nplayers=4):
         """A simple constructor, performs some sanity checks"""
         if nplayers < 2 or nplayers > 4: raise 'Barrier length must be >0 and <side length'
-        Board.__init__(self,side)
-        self.pp = [Pawn(self.middle, 0, '1', down)]
-        self.nplayers=nplayers
-        if self.nplayers == 2:
-            self.pp.append(Pawn(self.middle, self.side-1, '2', up))
-        elif self.nplayers == 3:
-            self.pp.append(Pawn(self.side-1, self.middle, '2', left))
-            self.pp.append(Pawn(self.middle, self.side-1, '3', up))
-        elif self.nplayers == 4:
-            self.pp.append(Pawn(self.side-1, self.middle, '2', left))
-            self.pp.append(Pawn(self.middle, self.side-1, '3', up))
-            self.pp.append(Pawn(0, self.middle, '4', right))
+        quoboard.Board.__init__(self,side)
+
+        initialised=False
+        while(not initialised):
+            self.pp = [Pawn(self.middle, 0, '1', down)]
+            self.nplayers=nplayers
+            if self.nplayers == 2:
+                self.pp.append(Pawn(self.middle, self.side-1, '2', up))
+            elif self.nplayers == 3:
+                self.pp.append(Pawn(self.side-1, self.middle, '2', left))
+                self.pp.append(Pawn(self.middle, self.side-1, '3', up))
+            elif self.nplayers == 4:
+                self.pp.append(Pawn(self.side-1, self.middle, '2', left))
+                self.pp.append(Pawn(self.middle, self.side-1, '3', up))
+                self.pp.append(Pawn(0, self.middle, '4', right))
+            # Check that the hashes are all different
+            # (usually overkill, but anyway...)
+            initialised=True
+            hs = [ p.h for p in self.pp ]
+            for h in hs:
+                if hs.count(h) > 1: initialised=False
+
 
 
 MyServerBoard=ServerBoard()
-MyServerBoard.addBarrier(Barrier(2,3,down,2))
+MyServerBoard.addBarrier(quoboard.Barrier(2,3,down,2))
 MyServerBoard.prettyPrintASCII()
+for p in MyServerBoard.pp: print p.h
